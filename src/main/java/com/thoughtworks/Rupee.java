3fd50a26b1d26b4d6d038edabfb8275ca4639d50
Megha Jagadale <megha.jagadale@thoughtworks.com>
2022-01-07 14:34:14 +0530

    Add. Logic to check two ten rupees are equals

--- src/main/java/com/thoughtworks/Rupee.java
+++ src/main/java/com/thoughtworks/Rupee.java
@@ -1,2 +1,28 @@
-package com.thoughtworks;public class Rupee {
+package com.thoughtworks;
+
+import java.util.Objects;
+
+public
+class Rupee {
+    private final int value;
+
+    public
+    Rupee(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public
+    boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Rupee)) return false;
+        Rupee rupee = (Rupee) o;
+        return value == rupee.value;
+    }
+
+    @Override
+    public
+    int hashCode() {
+        return Objects.hash(value);
+    }
 }
--- src/test/java/com/thoughtworks/RupeeTest.java
+++ src/test/java/com/thoughtworks/RupeeTest.java
@@ -1,2 +1,22 @@
-package com.thoughtworks;public class RupeeTest {
+package com.thoughtworks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+
+public
+class RupeeTest {
+
+    @Test
+    void shouldEquateWhenTenRupeeToTenRupeeIsGiven() {
+        int tenRupeeNote=10;
+
+        Rupee tenRupee = new Rupee(tenRupeeNote);
+        Rupee anotherTenRupee = new Rupee(tenRupeeNote);
+
+        assertThat(tenRupee, is(equalTo(anotherTenRupee)));
+    }
 }
