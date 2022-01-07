1b6c5b68321bd1b7208f783003d0c91e18d5e392
Megha Jagadale <megha.jagadale@thoughtworks.com>
2022-01-07 15:14:55 +0530

    Add. Logic to check two ten rupees are not equals

--- src/test/java/com/thoughtworks/RupeeTest.java
+++ src/test/java/com/thoughtworks/RupeeTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 
 public
@@ -18,5 +19,15 @@ class RupeeTest {
         Rupee anotherTenRupee = new Rupee(tenRupeeNote);
 
         assertThat(tenRupee, is(equalTo(anotherTenRupee)));
+    }
+
+    @Test
+    void shouldNotEquateWhenTenRupeeToTenRupeeIsGiven() {
+        int tenRupeeNote=10;
+
+        Rupee tenRupee = new Rupee(tenRupeeNote);
+        Rupee anotherTenRupee = new Rupee(tenRupeeNote);
+
+       assertNotSame(tenRupee,anotherTenRupee);
     }
 }
